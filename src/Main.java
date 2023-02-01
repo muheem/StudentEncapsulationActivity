480abcda71fbad4e46d902031db6e9a7a325294a
ibrahim rahman <heemee@gmail.com>
2023-02-01 17:33:27 +0000

    Added test cases. Builds and runs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MathsStudent student = new MathsStudent("Ashik Malik", 'C', 4);
+        try {
+            MathsStudent student2 = new MathsStudent("Ashik Malik", 'C', 7);
+        } catch (Exception e) {
+            System.out.println("Invalid Group value entered for student.");
+        }
+
+        System.out.println( student.getName());
+        student.upgrade();
+        System.out.println("Grade = " + student.getGrade());
+
+        student.downgrade();
+        student.downgrade();
+        System.out.println("Grade = " + student.getGrade());
+
+        // Check lowest grade is F
+        student.downgrade();
+        student.downgrade(); // reashed F and downgrading again...
+        student.downgrade();
+        System.out.println("Grade = " + student.getGrade());
+
+        // Check highest grade is A
+        student.upgrade();
+        student.upgrade();
+        student.upgrade();
+        student.upgrade();
+        student.upgrade(); //reached A and upgrading again...
+        student.upgrade();
+        System.out.println("Grade = " + student.getGrade());
     }
 }
+
--- src/MathsStudent.java
+++ src/MathsStudent.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class MathsStudent {
+public class MathsStudent {
+    private String name;
+    private char grade;
+    private int group;
+    private String secretNickName = "MySecretNickName";
+    private static char MAX_GRADE = 'A';
+    private static char MIN_GRADE = 'F';
+
+    private static int MAX_GROUP_VALUE = 5;
+    private static int MIN_GROUP_VALUE = 1;
+
+    public MathsStudent(String name, char grade, int group) {
+        GroupValueCheck(group);
+        this.name = name;
+        this.grade = grade;
+        this.group = group;
+    }
+
+    void GroupValueCheck(int group) {
+        if ((group < MIN_GROUP_VALUE) || (group > MAX_GROUP_VALUE) )
+            throw new IllegalArgumentException();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public char getGrade(){
+        return grade;
+    }
+    public void upgrade() {
+        if (grade != MAX_GRADE)
+            grade--;
+    }
+    public void downgrade() {
+        if (grade != MIN_GRADE)
+            grade++;
+    }
+    public int getGroup() {
+        return group;
+    }
+
 }
